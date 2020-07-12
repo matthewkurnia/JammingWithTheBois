c1f900a23dcb0f4486bc47f15a6149a367ccf90d
Matthew Kurnia <matthewkurnia@gmail.com>
2020-07-12 21:35:38 +0700

    new level, reworked UI

--- src/Buttons/ButtonAudioVisual.gd
+++ src/Buttons/ButtonAudioVisual.gd
@@ -1,8 +1,11 @@
-extends Sprite
+extends Node2D
 
 onready var button_audio = $ButtonAudio
 onready var pressed = $Pressed
 onready var released = $Released
+
+func _ready():
+	owner.connect("update_state", self, "update_pressed")
 
 func update_pressed(value: bool):
 	if value:
